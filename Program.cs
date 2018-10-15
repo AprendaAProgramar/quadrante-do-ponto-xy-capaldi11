702253f828e98bd59d2a2d40f0865331ac6f0e56
capaldi11 <43537137+capaldi11@users.noreply.github.com>
2018-10-15 12:14:05 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,8 +10,31 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //Coloque aqui o seu codigo
-        
+            //Quadrante do Ponto XY 
+            //Verifique em que quadrante do referencial cartesiano se encontra um ponto com as corrdenas (x,y) introduzidas pelo utilizador
+
+            Console.WriteLine("Insira a coordenada X");
+
+            double X = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Insira a coordenada Y");
+
+            double Y = Convert.ToDouble(Console.ReadLine());
+
+            if (X > 0 && Y > 0)
+                Console.WriteLine("O ponto esta no primeiro quadrante");
+            else
+                if (X < 0 && Y > 0)
+                Console.WriteLine("O ponto esta no segundo quadrante");
+                else
+                    if (X < 0 && Y < 0)
+                        Console.WriteLine("O ponto esta no terceiro quadrante");
+                    else
+                        if (X > 0 && Y < 0)
+                            Console.WriteLine("O ponto esta no quarto quadrante");
+            
+            
+            Console.ReadLine();
         }
     }
 }
